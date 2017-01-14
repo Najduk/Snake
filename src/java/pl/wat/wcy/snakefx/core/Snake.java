c9d17c3ef5bc6be81ec9095724260928601cdb5f
Yasper8 <marcinnajduk@interia.pl>
2017-01-14 04:29:31 +0100

    Snake
    Rebild oraz rozbudowa węża.

--- src/java/pl/wat/wcy/snakefx/core/Snake.java
+++ src/java/pl/wat/wcy/snakefx/core/Snake.java
@@ -1,205 +1,123 @@
 package pl.wat.wcy.snakefx.core;
 
-import pl.wat.wcy.snakefx.core.Direction;
-import pl.wat.wcy.snakefx.core.Field;
-import pl.wat.wcy.snakefx.core.GameLoop;
-import pl.wat.wcy.snakefx.core.Grid;
-import pl.wat.wcy.snakefx.core.Snake;
-import pl.wat.wcy.snakefx.core.State;
+import static pl.wat.wcy.snakefx.config.Config.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import pl.wat.wcy.snakefx.viewmodel.ViewModel;
 
 
-import org.mockito.internal.util.reflection.Whitebox;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 public class Snake {
-	private Snake snake;
 
-	private Grid gridMock;
+    private Field head;
 
-	private GameLoop gameLoopMock;
+    private final Grid grid;
 
-	private static final int X = 4;
-	private static final int Y = 2;
+    private final int x;
+    private final int y;
 
-	private ViewModel viewModel;
+    private Direction currentDirection;
 
-	public void setUp() {
-		gridMock = mock(Grid.class);
-		gameLoopMock = mock(GameLoop.class);
+    private Direction nextDirection;
 
-		viewModel = new ViewModel();
+    private final List<Field> tail;
 
-		snake = new Snake(viewModel, gridMock, gameLoopMock);
-		Whitebox.setInternalState(snake, "x", X);
-		Whitebox.setInternalState(snake, "y", Y);
 
-	}
+    private final ViewModel viewModel;
 
-	public void Initialization() {
-		final Field field = mock(Field.class);
+    public Snake(final ViewModel viewModel, final Grid grid, final GameLoop gameLoop) {
+        this.viewModel = viewModel;
+        this.grid = grid;
+        x = SNAKE_START_X.get();
+        y = SNAKE_START_Y.get();
 
-		when(gridMock.getXY(X, Y)).thenReturn(field);
+        tail = new ArrayList<Field>();
 
-		snake.init();
+        gameLoop.addActions(x -> {
+            move();
+        });
 
-		verify(gridMock, times(1)).getXY(X, Y);
-		verify(field, times(1)).changeState(State.HEAD);
+        viewModel.snakeDirection.addListener( (observable,oldDirection,newDirection) -> {
+            Snake.this.changeDirection(newDirection);
+        });
+    }
 
-		assertThat(getHead()).isEqualTo(field);
 
-		final Direction direction = currentDirectionFromSnake();
-		assertThat(direction).isEqualTo(Direction.UP);
-	}
+    public void init() {
+        setHead(grid.getXY(x, y));
 
-	public void ChangeDirection() {
-		viewModel.snakeDirection.set(Direction.LEFT);
-		final Direction direction = nextDirectionFromSnake();
+        viewModel.collision.set(false);
 
-		assertThat(direction).isEqualTo(Direction.LEFT);
-	}
+        viewModel.points.set(0);
 
-	public void ChangeDirectionNewHasSameOrientationAsOld() {
-		final Field head = mock(Field.class);
-		when(gridMock.getXY(X, Y)).thenReturn(head);
+        currentDirection = Direction.UP;
+        nextDirection = Direction.UP;
+    }
 
-		final Field newHead = mock(Field.class);
-		when(newHead.getState()).thenReturn(State.EMPTY);
-		when(gridMock.getFromDirection(head, Direction.LEFT)).thenReturn(newHead);
 
-		snake.init();
+    private void changeDirection(final Direction newDirection) {
+        if (!newDirection.hasSameOrientation(currentDirection)) {
+            nextDirection = newDirection;
+        }
+    }
 
-		viewModel.snakeDirection.set(Direction.DOWN);
 
-		assertThat(nextDirectionFromSnake()).isEqualTo(Direction.UP);
-		assertThat(currentDirectionFromSnake()).isEqualTo(Direction.UP);
+    void move() {
+        currentDirection = nextDirection;
 
-		viewModel.snakeDirection.set(Direction.LEFT);
+        final Field newHead = grid.getFromDirection(head, currentDirection);
 
-		assertThat(nextDirectionFromSnake()).isEqualsToByComparingFields(Direction.LEFT);
-		
-		assertThat(currentDirectionFromSnake()).isEqualTo(Direction.UP);
+        if (newHead.getState().equals(State.TAIL)) {
+            viewModel.collision.set(true);
+            return;
+        }
 
-		viewModel.snakeDirection.set(Direction.DOWN);
+        boolean grow = false;
+        if (newHead.getState().equals(State.FOOD)) {
+            grow = true;
+        }
 
-		assertThat(nextDirectionFromSnake()).isEqualTo(Direction.LEFT);
-		assertThat(currentDirectionFromSnake()).isEqualTo(Direction.UP);
+        Field lastField = head;
 
-		snake.move();
+        for (int i = 0; i < tail.size(); i++) {
+            final Field f = tail.get(i);
 
-		assertThat(nextDirectionFromSnake()).isEqualTo(Direction.LEFT);
+            lastField.changeState(State.TAIL);
+            tail.set(i, lastField);
 
-		assertThat(currentDirectionFromSnake()).isEqualTo(Direction.LEFT);
-	}
+            lastField = f;
+        }
 
-	public void Move() {
-		final Field oldHead = mock(Field.class);
-		when(oldHead.getState()).thenReturn(State.EMPTY);
-		when(gridMock.getXY(X, Y)).thenReturn(oldHead);
+        if (grow) {
+            grow(lastField);
+            addPoints();
+        } else {
+            lastField.changeState(State.EMPTY);
+        }
 
-		snake.init();
+        setHead(newHead);
+    }
 
-		final Field newHead = mock(Field.class);
-		when(newHead.getState()).thenReturn(State.EMPTY);
-		when(gridMock.getFromDirection(oldHead, Direction.UP)).thenReturn(newHead);
+    public void newGame() {
+        tail.clear();
+        init();
+    }
 
-		snake.move();
+    private void setHead(final Field head) {
+        this.head = head;
+        head.changeState(State.HEAD);
+    }
 
-		assertThat(getHead()).isEqualTo(newHead);
+    private void grow(final Field field) {
+        field.changeState(State.TAIL);
+        tail.add(field);
+    }
 
-		verify(oldHead).changeState(State.EMPTY);
-	}
+    private void addPoints() {
+        final int current = viewModel.points.get();
+        viewModel.points.set(current + 1);
+    }
 
 
-	public void Grow() {
-		final Field field1 = new Field(0, 3, 10);
-
-		when(gridMock.getXY(X, Y)).thenReturn(field1);
-
-		final Field field2 = new Field(0, 2, 10);
-		field2.changeState(State.FOOD);
-		when(gridMock.getFromDirection(field1, Direction.UP)).thenReturn(field2);
-
-		final Field field3 = new Field(0, 1, 10);
-		when(gridMock.getFromDirection(field2, Direction.UP)).thenReturn(field3);
-
-		snake.init();
-
-		snake.move();
-
-		assertThat(getHead()).isEqualTo(field2);
-
-		assertThat(field1.getState()).isEqualTo(State.TAIL);
-
-		assertThat(viewModel.points.get()).isEqualTo(1);
-
-		snake.move();
-
-		assertThat(getHead()).isEqualTo(field3);
-
-		assertThat(field2.getState()).isEqualTo(State.TAIL);
-
-		assertThat(field1.getState()).isEqualTo(State.EMPTY);
-	}
-
-	public void Collision() {
-
-		final Field oldHead = mock(Field.class);
-		when(oldHead.getState()).thenReturn(State.EMPTY);
-		when(gridMock.getXY(X, Y)).thenReturn(oldHead);
-
-		snake.init();
-
-		final Field tail = mock(Field.class);
-		when(tail.getState()).thenReturn(State.TAIL);
-		when(gridMock.getFromDirection(oldHead, Direction.UP)).thenReturn(tail);
-
-		snake.move();
-
-		assertThat(viewModel.collision.get()).isTrue();
-	}
-
-	public void NewGame() {
-		final Field head = mock(Field.class);
-		when(head.getState()).thenReturn(State.EMPTY);
-		when(gridMock.getXY(X, Y)).thenReturn(head);
-
-		final Field food = mock(Field.class);
-		when(food.getState()).thenReturn(State.FOOD);
-		when(gridMock.getFromDirection(head, Direction.UP)).thenReturn(food);
-
-		snake.init();
-		snake.move();
-
-		assertThat(getHead()).isEqualTo(food);
-		assertThat(getTail()).hasSize(1);
-		assertThat(getTail().contains(head));
-
-		snake.newGame();
-
-		assertThat(getHead()).isEqualTo(head);
-		assertThat(getTail()).isEmpty();
-
-	}
-
-	@SuppressWarnings("unchecked")
-	private List<Field> getTail() {
-		return (List<Field>) Whitebox.getInternalState(snake, "tail");
-	}
-
-	private Field getHead() {
-		return (Field) Whitebox.getInternalState(snake, "head");
-	}
-
-	private Direction nextDirectionFromSnake() {
-		return (Direction) Whitebox.getInternalState(snake, "nextDirection");
-	}
-
-	private Direction currentDirectionFromSnake() {
-		return (Direction) Whitebox.getInternalState(snake, "currentDirection");
-	}
 }
