a2426e6c0175948c56790c72c6e69e1862584d47
WojtekOrych <marcinnajduk@wp.pl>
2017-01-14 05:57:39 +0100

    Launcher

---
+++ src/java/pl/wat/wcy/snakefx/Launcher.java
@@ -0,0 +1,46 @@
+package pl.wat.wcy.snakefx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import pl.wat.wcy.snakefx.inject.DependencyInjector;
+import pl.wat.wcy.snakefx.util.FxmlFactory;
+import pl.wat.wcy.snakefx.util.KeyboardHandler;
+import pl.wat.wcy.snakefx.util.PopupDialogHelper;
+import pl.wat.wcy.snakefx.view.FXMLFile;
+import pl.wat.wcy.snakefx.viewmodel.ViewModel;
+
+public class Launcher extends Application {
+
+    public static void main(final String... args) {
+        Application.launch(Launcher.class, args);
+    }
+
+
+    @Override
+    public void start(final Stage primaryStage) {
+        DependencyInjector dependencyInjector = new DependencyInjector();
+
+        FxmlFactory fxmlFactory = new FxmlFactory(dependencyInjector);
+        Scene scene = new Scene(fxmlFactory.getFxmlRoot(FXMLFile.MAIN));
+        scene.setOnKeyPressed(dependencyInjector.get(KeyboardHandler.class));
+
+
+        PopupDialogHelper popupDialogHelper = new PopupDialogHelper(fxmlFactory);
+
+
+        ViewModel viewModel = dependencyInjector.get(ViewModel.class);
+
+        Stage aboutStage = popupDialogHelper.createModalDialog(viewModel.aboutWindowOpen, primaryStage, FXMLFile.ABOUT);
+
+        Stage highScoreStage = popupDialogHelper.createModalDialog(viewModel.highscoreWindowOpen, primaryStage, FXMLFile.HIGHSCORE);
+
+        popupDialogHelper.createModalDialog(viewModel.newHighscoreWindowOpen, highScoreStage, FXMLFile.NEW_HIGHSCORE);
+
+
+        primaryStage.setTitle("SnakeFX");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+}
